5560f0ff84bd7c88441d7164d309d9841f174368
dj <deepakjayswal22@gmail.com>
2024-10-22 19:30:31 +0530

    auth and user routes done

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,10 +4,6 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
-  changeCurrentPassword,
-  // getCurrentUser,
-  updateUserAvatar,
-  updateAccountDetails,
 } from "../controllers/auth.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
@@ -29,11 +25,5 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-
-router
-  .route("/avatar")
-  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
 export default router;
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,19 +1,30 @@
 import express from "express";
 import {
   getUser,
+  changeCurrentPassword,
+  updateUser,
+  updateAvatar,
   // updateUserStatus,
   // getUserContacts,
   myProfile,
 } from "../controllers/user.controller.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
+import {upload} from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 // for self profile
-router.route("/me").get(verifyJWT, myProfile);
+router.route("/me").get(verifyJWT, myProfile); // tested
 
 // GET /api/users/:id - Get user details by ID
-router.route("/:id").get(verifyJWT, getUser);
+router.route("/:id").get(verifyJWT, getUser); // tested
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword); // tested
+router.route("/update/me/:id").patch(verifyJWT, updateUser); // tested
+
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar); // tested
 
 // PUT /api/users/:id/status - Update user status (online/offline, last seen)
 // router.route("/:id/status").put(verifyJWT, updateUserStatus);
