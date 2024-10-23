6f18a904602b52e3f223eb3150c438948830c0c4
dj <deepakjayswal22@gmail.com>
2024-10-23 11:00:45 +0530

    testing done

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -10,6 +10,7 @@ import {verifyJWT} from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// tested
 router.route("/register").post(
   upload.fields([
     {
@@ -20,10 +21,10 @@ router.route("/register").post(
   registerUser
 );
 
-router.route("/login").post(loginUser);
+router.route("/login").post(loginUser); // tested
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken);
+router.route("/logout").post(verifyJWT, logoutUser); // tested
+router.route("/refresh-token").post(refreshAccessToken); // tested
 
 export default router;
