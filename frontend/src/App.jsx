470b0a1909b9abd469b3c253d1be2da6f90825ad
dj <deepakjayswal22@gmail.com>
2024-10-27 18:42:15 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import {useState} from "react";
 
 import "./App.css";
+import {initializeSocketListeners} from "./store/slices/chatSlice";
+import {useDispatch} from "react-redux";
+import dotenv from "dotenv";
+dotenv.config();
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Initialize Socket.IO listeners for real-time updates
+    initializeSocketListeners(dispatch);
+  }, [dispatch]);
+
   return (
     <>
       <div>app</div>
     </>
   );
-}
+};
 
 export default App;
