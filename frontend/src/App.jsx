c636f717ef018b269cdcc5774ac1d78002defa20
dj <deepakjayswal22@gmail.com>
2024-10-28 21:16:47 +0530

    login functionality completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,52 @@
-import {useState} from "react";
+// useEffect(() => {
+//   // Initialize Socket.IO listeners for real-time updates
+//   initializeSocketListeners(dispatch);
+// }, [dispatch]);
 
+// src/App.jsx
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+// import authSlice from "./redux/slices/auth.slice.js";
+import {useSocket} from "./hooks/useSocket.hook.js";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 import "./App.css";
-import {initializeSocketListeners} from "./store/slices/chatSlice";
-import {useDispatch} from "react-redux";
-import dotenv from "dotenv";
-dotenv.config();
 
 const App = () => {
   const dispatch = useDispatch();
+  const {isAuthenticated} = useSelector((state) => state.auth);
+  console.log("isAuthenticated: ", isAuthenticated);
+  const socket = useSocket();
+
+  // useEffect(() => {
+  //   dispatch(checkAuthStatus()); // Check if user is logged in on component mount
+  // }, [dispatch]);
 
   useEffect(() => {
-    // Initialize Socket.IO listeners for real-time updates
-    initializeSocketListeners(dispatch);
-  }, [dispatch]);
+    if (isAuthenticated && socket) {
+      socket.connect(); // Connect to socket only if authenticated
+    }
+  }, [isAuthenticated, socket]);
 
   return (
-    <>
-      <div>app</div>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />}
+        />
+        <Route
+          path="/"
+          element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </Router>
   );
 };
 
