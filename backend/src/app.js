de17641e17228200b030aaa58259b2f487ab8a73
dj <deepakjayswal22@gmail.com>
2024-11-04 09:41:01 +0530

    tailwind added

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,19 +4,17 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-// line 7 to 20 all are middleware connections . registering or connecting middleware to express
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    // origin: [process.env.FRONTEND_URL],
-    methods: ["POST", "GET", "PUT", "DELETE"], //only these methods are allowed
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true, //to make fronted and backend connection easier
   })
 );
 
 app.use(express.json({limit: "16kb"})); //to parse incoming JSON data in the request body.
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
-// app.use(express.static("public"))
+
 app.use(cookieParser()); //parses cookies from the incoming request
 
 //routes import
@@ -25,6 +23,6 @@ import allRoutes from "./routes/index.routes.js";
 //routes declaration
 app.use("/api/v1", allRoutes);
 
-// http://localhost:8000/api/v1/users/register
+// http://localhost:7000/api/v1/users/register
 
 export {app};
